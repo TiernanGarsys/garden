ad483c35a1e7f140c19cd75d55207b166d251321
Tiernan Garsys <tgarsys@mac.com>
2021-12-30 15:34:15 -0500

    create-react-app, add pixi js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import * as PIXI from "pixi.js";
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    // On first render create our application
+    const app = new Application({
+      width: 800,
+      height: 600,
+      backgroundColor: 0x5BBA6F,
+    });
+
+    // Add app to DOM
+    ref.current.appendChild(app.view);
+    // Start the PixiJS app
+    app.start();
+
+    return () => {
+      // On unload completely destroy the application and all of it's children
+      app.destroy(true, true);
+    };
+  }, []);
+ 
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
