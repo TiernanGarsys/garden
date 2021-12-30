63e9c70917790fba09c39e08953545af3b4f6cdf
Tiernan Garsys <tgarsys@mac.com>
2021-12-30 16:11:41 -0500

    bootstrap pixi.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,25 @@
-import * as PIXI from "pixi.js";
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { Application } from "pixi.js";
 
 function App() {
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // On first render create our application
     const app = new Application({
       width: 800,
       height: 600,
       backgroundColor: 0x5BBA6F,
     });
 
-    // Add app to DOM
-    ref.current.appendChild(app.view);
-    // Start the PixiJS app
+    ref.current!.appendChild(app.view);
     app.start();
 
     return () => {
-      // On unload completely destroy the application and all of it's children
       app.destroy(true, true);
     };
   }, []);
  
-  return (
-    <div className="App" >
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div ref={ref} />;
 }
 
 export default App;
