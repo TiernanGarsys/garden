4b9c90a9ae632e5abaae45b98247ac435261352c
Tiernan Garsys <tgarsys@mac.com>
2022-01-09 16:17:09 -0500

    add positional update loop

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -11,17 +11,17 @@ interface DisplayProps {
 
 interface AgentFrame {
   id: AgentId,
-  frame: Graphics,
+  gfx: Graphics,
 }
 
 interface NodeFrame {
   id: NodeId,
-  frame: Graphics,
+  gfx: Graphics,
 }
 
 interface EdgeFrame {
   id: EdgeId,
-  frame: Graphics,
+  gfx: Graphics,
 }
 
 interface DisplayState {
@@ -53,24 +53,37 @@ function Display(props: DisplayProps) {
     app.stage.addChild(frame);
 
     const processSimUpdate= (update: SimUpdate) => {
-      for (let id in update.addedAgents) {
-        agents.set(id, {
+      console.log(`processing update { update }`);
+      for (let id of update.addedAgents) {
+        let frame = {
           id: id,
-          frame: new Graphics(),
-        });
+          gfx: new Graphics(),
+        };
+        frame.gfx.beginFill(props.settings.palette.agent[0]);
+        frame.gfx.position.set(-100, -100);
+        agents.set(id, frame);
+        app.stage.addChild(frame.gfx);
       }
-      for (let id in update.addedNodes) {
-        nodes.set(id, {
+      for (let id of update.addedNodes) {
+        let frame = {
           id: id,
-          frame: new Graphics(),
-
-        });
+          gfx: new Graphics(),
+        };
+        frame.gfx.beginFill(props.settings.palette.node);
+        frame.gfx.position.set(-100, -100);
+        nodes.set(id, frame);
+        app.stage.addChild(frame.gfx);
       }
-      for (let id in update.addedEdges) {
-        edges.set(id, {
+      for (let id of update.addedEdges) {
+        let frame = {
           id: id,
-          frame: new Graphics(),
-        });
+          gfx: new Graphics(),
+        };
+        frame.gfx.beginFill(props.settings.palette.edge);
+        frame.gfx.lineStyle({ color: props.settings.palette.edge, width: 2, alignment: 0 });
+        frame.gfx.position.set(-100, -100);
+        edges.set(id, frame);
+        app.stage.addChild(frame.gfx);
       }
       for (let id in update.removedAgents) {
         agents.delete(id);
@@ -83,16 +96,44 @@ function Display(props: DisplayProps) {
       }
     }
 
+    const updatePositions = () => {
+      console.log('Updating positions');
+      const height = window.innerHeight;
+      const width = window.innerWidth;
+      edges.forEach((frame, id) => {
+        console.log(id);
+        const seg = state.sim.getEdgePosition(id);
+        frame.gfx.clear();
+        frame.gfx.position.set(seg[0][0] * width, seg[0][1] * height);
+        frame.gfx.lineTo(seg[1][0] * width, seg[1][1] * height);
+      });
+      nodes.forEach((frame, id) => {
+        console.log(`Updating Node { id }`);
+        const pos = state.sim.getNodePosition(id);
+        frame.gfx.position.set(pos[0] * width, pos[1] * height);
+      });
+      agents.forEach((frame, id) => {
+        const pos = state.sim.getAgentPosition(id);
+        frame.gfx.position.set(pos[0] * width, pos[1] * height);
+      });
+    }
+
+    let elapsed = 0
     let initialTick = true;
     app.ticker.add((delta) => {
+      frame.position.set(100.0 + Math.cos(elapsed/50.0) * 100.0, 200);
+      elapsed += delta;
+
       let update: SimUpdate;
       if (initialTick) {
         update = state.sim.initTest();
+        initialTick = false;
       } else {
         update = state.sim.tick(delta);
       }
 
       processSimUpdate(update);
+      updatePositions();
     });
 
 
--- src/core/Sim.ts
+++ src/core/Sim.ts
@@ -4,6 +4,7 @@ export type AgentId = string;
 export type EdgeId = string;
 export type NodeId = string;
 export type Point = [number, number];
+export type Segment = [Point, Point];
 
 export interface SimUpdate {
   addedAgents: AgentId[],
@@ -17,9 +18,9 @@ export interface SimUpdate {
 interface Agent {
   id: AgentId,
   position: Point,
-  finalDest: number,
-  currentDest: number,
-  currentEdge: number,
+  finalDest: NodeId,
+  currentDest: NodeId,
+  currentEdge: EdgeId,
 }
 
 interface Node {
@@ -31,42 +32,42 @@ interface Node {
 interface Edge {
   id: EdgeId,
   src: NodeId,
-  dest: NodeId,
+  dst: NodeId,
 }
 
 class Sim {
   settings: Settings;
-  agents: Agent[]; 
-  nodes: Node[];
-  edges: Edge[];
+  agents: Map<AgentId, Agent>;
+  nodes: Map<NodeId, Node>;
+  edges:Map<EdgeId, Edge>; 
 
   constructor(settings: Settings) {
     this.settings = settings;
-    this.agents = [];
-    this.nodes = [];
-    this.edges = [];
+    this.agents = new Map<AgentId, Agent>();
+    this.nodes = new Map<NodeId, Node>();
+    this.edges = new Map<EdgeId, Edge>();
   }
 
   initTest() {
-    this.nodes.push({
+    this.nodes.set("N1", {
       id: "N1",
       position: [0.25, 0.5],
       edges: ["E1", "E2"],
     });
-    this.nodes.push({
+    this.nodes.set("N2", {
       id: "N2",
       position: [0.75, 0.5],
       edges: ["E1", "E2"],
     });
-    this.edges.push({
+    this.edges.set("E1", {
       id: "E1",
       src: "N1",
-      dest: "N2",
+      dst: "N2",
     });
-    this.edges.push({
+    this.edges.set("E2", {
       id: "E2",
       src: "N2",
-      dest: "N1",
+      dst: "N1",
     });
 
     return {
@@ -83,15 +84,44 @@ class Sim {
     this.settings = settings;
   }
 
+  getAgentPosition(id: AgentId): Point { 
+    const agent = this.agents.get(id);
+    if (agent) {
+      return agent.position;
+    } else {
+      throw new Error(`No agent with ID: ${ id }`)
+    }
+  }
+
+  getNodePosition(id: NodeId): Point { 
+    const node = this.nodes.get(id);
+    if (node) {
+      return node.position;
+    } else {
+      throw new Error(`No node with ID: ${ id }`)
+    }
+  }
+
+  getEdgePosition(id: EdgeId): Segment { 
+    const edge = this.edges.get(id);
+    if (edge) {
+      const p1 = this.getNodePosition(edge.src);
+      const p2 = this.getNodePosition(edge.dst);
+      return [p1, p2];
+    } else {
+      throw new Error(`No edge with ID: ${ id }`)
+    }
+  }
+
   tick(delta: number): SimUpdate {
 
     // TODO(tiernan): Check whether we should remove nodes ()
     // TODO(tiernan): Check whether we should remove edge (references null node)
 
     for (let agent in this.agents) {
-      // TODO(tiernan): If agent is at final destination, delete
-      // TODO(tiernan): If agent is at current destination, choose new one
-      // TODO(tiernan): ELSE: move agent toward current destination at speed
+      // TODO(tiernan): If agent is at final dstination, delete
+      // TODO(tiernan): If agent is at current dstination, choose new one
+      // TODO(tiernan): ELSE: move agent toward current dstination at speed
       //  informed by edge being used for travel
     }
 
